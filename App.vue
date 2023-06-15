4baa5863e6c523bb18dbba983f8df15264c059ef
georgeCx5 <69874793+georgeCx5@users.noreply.github.com>
2023-06-15 00:33:58 -0500

    Add files via upload

--- App.vue
+++ App.vue
@@ -52,7 +52,7 @@ export default {
 </script>
 <template>
   <body class="flex flex-col items-center text-neo-violet font-karla">
-    <nav class=" flex justify-between items-center w-full h-20 bg-white px-6 DT:z-50">
+    <nav class=" flex justify-between items-center w-full h-20 bg-white px-6 TB:px-10 DT:px-24 DTL:px-40 DTX:px-44 DT:z-50">
       <Logo />
       <!-- Nav Mobile -->
       <button @click="barClosed = !barClosed" class=" TB:hidden cursor-pointer">
@@ -68,7 +68,7 @@ export default {
         <BgPatternNav class=" absolute right-0 bottom-0 pointer-events-none" />
       </div>
       <!-- Nav Tablet/Desktop -->
-      <div class=" hidden TB:flex TB:items-center TB:gap-[20px] DT:gap-[25px]">
+      <div class=" hidden TB:flex TB:items-center TB:gap-[20px] DT:gap-[25px] DTL:gap-[28px] DTX:gap-10">
         <NavNoBarBtn txt="How we work" />
         <NavNoBarBtn txt="Blog" />
         <NavNoBarBtn txt="Account" />
@@ -76,11 +76,11 @@ export default {
       </div>
     </nav>
     <main class=" flex flex-col gap-[140px] DT:gap-[158px] w-[375px] TB:w-[768px] DT:w-full mb-[140px] DT:mb-[150px]">
-      <div class=" DT:relative TB:flex TB:h-[451px] DT:h-[540px] DT:mb-[148px]">
+      <div class=" DT:relative TB:flex TB:h-[451px] DT:h-[540px] DTL:h-[600px] DT:mb-[148px]">
         <img class=" DT:hidden" :src="imgMobile" alt="imgMobile" draggable="false">
-        <img class=" DT:absolute DT:top-[105px] DT:right-[115px] hidden DT:block DT:w-[459px] DT:z-40 " :src="imgDesktop"
-          alt="imgDesktop" draggable="false">
-        <div class=" relative DT:w-full px-6 DT:px-[115px] bg-neo-violet text-neo-light-gray text-center DT:text-left">
+        <img class=" DT:absolute DT:top-[105px] DT:right-[115px] DTL:right-[165px] DTX:right-[195px] hidden DT:block DT:w-[459px] DTL:w-[540px] DTX:w-[621px] DT:z-40 "
+          :src="imgDesktop" alt="imgDesktop" draggable="false">
+        <div class=" relative DT:w-full px-6 DT:px-[115px] DTL:px-[165px] DTX:px-[195px] bg-neo-violet text-neo-light-gray text-center DT:text-left">
           <div class=" absolute left-0 top-0 DT:top-[428px] pointer-events-none">
             <BgPatternIntroLeftMobile class=" DT:hidden" />
             <BgPatternIntroLeftDesktop class=" hidden DT:block" />
@@ -90,10 +90,10 @@ export default {
             <BgPatternIntroRightDesktop class=" hidden DT:block" />
           </div>
           <div
-            class=" DT:relative flex flex-col justify-center gap-4 items-center DT:items-start DT:w-[490px] h-[500px] TB:h-full">
+            class=" DT:relative flex flex-col justify-center items-center DT:items-start gap-4 DTX:gap-6 DT:w-[490px] DTL:w-[540px] DTX:w-[625px] h-[500px] TB:h-full">
             <div class=" absolute top-[104px] hidden DT:block w-[150px] h-[1px] bg-white "></div>
             <h1
-              class=" text-[48px] DT:text-[56px] leading-[48px] DT:leading-[56px] tracking-[-.67px] DT:tracking-[-1px] font-serif-display z-30">
+              class=" text-[48px] DT:text-[64px] DTL:text-[72px] leading-[48px] DT:leading-[56px] tracking-[-.67px] DT:tracking-[-1px] font-serif-display z-30">
               Humanizing your insurance.
             </h1>
             <p class=" mb-4 DT:mb-2 px-[2px] text-[16px] leading-[26px]">
@@ -108,24 +108,26 @@ export default {
         </div>
       </div>
       <div class=" flex flex-col DT:items-center gap-20 text-center DT:text-left">
-        <div class=" flex flex-col items-center DT:items-start DT:w-[1110px] gap-10">
+        <div class=" flex flex-col items-center DT:items-start DT:w-[1110px] DTX:w-[1240px] gap-10">
           <div class=" w-[150px] h-[1px] bg-extra1"></div>
-          <h1 class=" text-[48px] leading-[48px] tracking-[-.67px] font-serif-display">We’re different</h1>
+          <h1
+            class=" text-[48px] DT:text-[64px] DTL:text-[72px] leading-[48px] DT:leading-[56px] DTL:leading-[64px] tracking-[-.67px] DT:tracking-[-1px] font-serif-display">
+            We’re different</h1>
         </div>
-        <div class=" flex flex-col DT:flex-row gap-14 DT:gap-[30px]">
+        <div class=" flex flex-col DT:flex-row gap-14 DT:gap-[30px] DTX:gap-16">
           <Features :index="0" icon="One" />
           <Features :index="1" icon="Two" />
           <Features :index="2" icon="Three" />
         </div>
       </div>
       <div
-        class=" relative flex flex-col TB:flex-row justify-center TB:justify-between items-center gap-10 h-[344px] TB:h-[250px] bg-neo-violet mx-6 TB:mx-10 DT:mx-[115px] TB:px-8 DT:px-16 text-neo-light-gray text-center DT:text-left">
+        class=" relative flex flex-col TB:flex-row justify-center TB:justify-between items-center gap-10 h-[344px] TB:h-[250px] bg-neo-violet mx-6 TB:mx-10 DT:mx-[115px] DTL:mx-[165px] DTX:mx-[215px] TB:px-8 DT:px-16 DTL:px-20 DTX:px-24 text-neo-light-gray text-center DT:text-left">
         <div class=" absolute right-0 top-0 pointer-events-none">
           <BgPatternHowWeWorkMobile class=" TB:hidden" />
           <BgPatternHowWeWorkDesktop class=" hidden TB:block" />
         </div>
         <h1
-          class=" TB:w-[415px] text-[40px] DT:text-[48px] leading-[40px] DT:leading-[48px] tracking-[-.56px] font-serif-display z-30">
+          class=" TB:w-[415px] DTL:w-[475px] text-[40px] DT:text-[48px] DTL:text-[56px] leading-[40px] DT:leading-[48px] DTL:leading-[56px] tracking-[-.56px] DT:tracking-[-.72px] font-serif-display z-30">
           Find out more about how we work</h1>
         <button
           class=" w-[160px] h-[40px] hover:bg-neo-light-gray hover:text-neo-dark-violet text-[13px] leading-[15px] tracking-[1.5px] font-bold uppercase border-[1.5px] border-neo-light-gray select-none z-30">How
@@ -133,7 +135,7 @@ export default {
       </div>
     </main>
     <footer
-      class=" relative flex flex-col gap-10 items-center w-full px-6 TB:px-20 DT:px-[115px] py-[88px] bg-neo-light-gray">
+      class=" relative flex flex-col gap-10 DT:gap-9 items-center w-full px-6 TB:px-12 DT:px-[115px] DTL:px-[165px] DTX:px-[215px] py-[88px] DT:py-16 bg-neo-light-gray">
       <div class=" absolute top-0 left-0 pointer-events-none">
         <BgPatternFooterMobile class=" TB:hidden" />
         <BgPatternFooterDesktop class=" hidden TB:block" />
@@ -149,7 +151,7 @@ export default {
       </div>
       <div class=" w-full h-[1px] bg-neo-grayish-violet TB:z-30"></div>
       <div
-        class=" flex flex-col TB:flex-row TB:justify-center DT:justify-start gap-10 TB:gap-16 DT:gap-44 w-[200px] TB:w-auto DT:w-full bg-red-200 text-center DT:text-left text-[13px] leading-[15px] tracking-[1.5px] font-bold TB:z-30">
+        class=" flex flex-col TB:flex-row TB:justify-center DT:justify-start gap-10 TB:gap-24 DT:gap-44 DTL:gap-48 DTX:gap-64 w-[200px] TB:w-full text-center DT:text-left text-[13px] leading-[15px] tracking-[1.5px] font-bold TB:z-30">
         <FooterText :index="0" />
         <FooterText :index="1" />
         <FooterText :index="2" />
